0ecf617b70e3dc3dccd6d04c2963af0b1e682af0
Rayane-hub <rayane.tisec77@gmail.com>
2026-02-12 18:17:19 +0100

    change no bear

--- script.js
+++ script.js
@@ -3,12 +3,10 @@ function clickfunc() {
 }
 
 function increaseSize() {
-    const yesButton = document.querySelector(".yes");
-
-    yesButton.style.width = (yesButton.offsetWidth + 70) + 'px';
-    yesButton.style.height = (yesButton.offsetHeight + 70) + 'px';
-
-    const currentFontSize = window.getComputedStyle(yesButton).fontSize;
-    const newFontSize = parseInt(currentFontSize) + 20 + 'px';
-    yesButton.style.fontSize = newFontSize
+    const bear = document.querySelector('.bear-img');
+    if (bear) {
+        // Remplacer l'image par un ours énervé (exemple de GIF libre)
+        bear.src = "https://img.over-blog-kiwi.com/0/98/03/83/20200529/ob_f5525c_abf1020a.gif";
+        bear.alt = "Ours en colère";
+    }
 }
